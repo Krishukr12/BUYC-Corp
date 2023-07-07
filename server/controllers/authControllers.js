ab347c24ea917b8f70884676966cbf6674441891
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-07-07 18:12:31 +0530

    Function added : Login and signup

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,9 +1,76 @@
-const registerUserController = (req, res, next) => {
-  res.send("Welcome to register page");
+// All imports
+const UserModel = require("../models/User.models");
+const bcrypt = require("bcrypt");
+const createError = require("../utils/createError");
+const jwt = require("jsonwebtoken");
+
+// Controller : Register user
+const registerUserController = async (req, res, next) => {
+  const { email, password, firstName, lastName } = req.body;
+  try {
+    const existingUser = await UserModel.findOne({ email: email });
+    if (existingUser) {
+      return next(createError(409, "User already exists"));
+    }
+
+    bcrypt.hash(password, 10, async (err, hash) => {
+      if (err) {
+        return next(createError(500, "Internal Server Error"));
+      }
+
+      const newUser = new UserModel({
+        email: email,
+        password: hash,
+        firstName: firstName,
+        lastName: lastName,
+      });
+
+      await newUser.save();
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+        user: newUser,
+      });
+    });
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
 };
 
-const loginUserController = (req, res, next) => {
-  res.send("Welcome to login page");
+// Controller : Login user
+const loginUserController = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email: email });
+    if (!user) {
+      return next(createError(401, "Invalid credentials"));
+    }
+
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err || !result) {
+        return next(createError(401, "Invalid credentials"));
+      }
+
+      const payload = { userId: user._id };
+      const secretKey = process.env.JWT_SECRET;
+
+      jwt.sign(payload, secretKey, { expiresIn: "1h" }, (err, token) => {
+        if (err) {
+          return next(createError(500, "Failed to generate JWT"));
+        }
+
+        const { _id, email, firstName, lastName } = user;
+        res.status(200).json({
+          success: true,
+          message: "User authenticated successfully",
+          user: { _id, email, firstName, lastName },
+          token: token,
+        });
+      });
+    });
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
 };
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -20,8 +20,17 @@ app.get("/", (req, res) => {
 // All Routes
 app.use("/user", authRouter);
 
-
-
+//  Middleware to handle error
+app.use((err, req, res, next) => {
+  const errStatus = err.status || 500;
+  const errMessage = err.message || "something went wrong";
+  res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMessage,
+    stack: err.stack,
+  });
+});
 
 app.listen(process.env.PORT, async () => {
   try {
